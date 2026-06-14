f41212422fafc1e16a74c6d43749fd2d37b3b1fb
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2026-06-14 16:08:40 -0300

    New core project to share API code
    - Created IS.Core.API to share API code related.

---
+++ src/shared/IS.Core.API/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using IS.Core.API.Results;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IS.Core.API.Controllers
+{
+    [ApiController]
+    public class BaseController : ControllerBase
+    {
+        protected IActionResult BadRequestProblem(IDictionary<string, ICollection<string>> errors)
+        {
+            return BadRequest(new BadRequestProblemDetails(errors, HttpContext));
+        }
+    }
+}
---
+++ src/shared/IS.Core.API/Results/BadRequestProblemDetails.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
+
+namespace IS.Core.API.Results
+{
+    public class BadRequestProblemDetails : ProblemDetails
+    {
+        [JsonPropertyName("errors")]
+        public IDictionary<string, ICollection<string>> Errors { get; }
+
+        public BadRequestProblemDetails(IDictionary<string, ICollection<string>> errors, HttpContext httpContext)
+        {
+            Type = "https://httpstatuses.com/400";
+            Title = "Invalid request data.";
+            Status = StatusCodes.Status400BadRequest;
+            Detail = "One or more business validation errors occurred.";
+            Instance = httpContext.Request.Path;
+            Errors = errors;
+            Extensions["traceId"] = httpContext.TraceIdentifier;
+        }
+    }
+}
