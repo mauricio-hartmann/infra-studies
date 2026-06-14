bb4ee8f79807f56114f87b3ceba33079a732d988
Mauricio Hartmann <mauricio.hartmann@outlook.com>
2026-06-14 16:09:24 -0300

    Update in BaseResult errors
    BaseResult errors changed to a dictionary

--- src/shared/IS.Core/Communication/BaseResult.cs
+++ src/shared/IS.Core/Communication/BaseResult.cs
@@ -2,11 +2,27 @@
 {
     public sealed class BaseResult<T>
     {
+        private const string _generalErrorKey = "General";
+
         public T? Response { get; set; }
-        public ICollection<string> Errors { get; init; } = [];
+        public IDictionary<string, ICollection<string>> Errors { get; init; } = new Dictionary<string, ICollection<string>>();
         public bool IsValid { get => Errors.Count == 0; }
 
-        public void AddError(string error) => Errors.Add(error);
+        public void AddError(string key, string error)
+        {
+            if (!Errors.TryGetValue(key, out ICollection<string> errorsList))
+            {
+                errorsList = new List<string>();
+                Errors.Add(key, errorsList);
+            }
+
+            errorsList.Add(error);
+        }
+
+        public void AddGeneralError(string error)
+        {
+            AddError(_generalErrorKey, error);
+        }
 
         public static BaseResult<T> Success(T response)
         {
@@ -16,12 +32,20 @@
             };
         }
 
-        public static BaseResult<T> Failure(params string[] errors)
+        public static BaseResult<T> Failure(IDictionary<string, string[]> errors)
         {
             return new BaseResult<T>
             {
-                Errors = errors
+                Errors = errors.ToDictionary(x => x.Key, x => (ICollection<string>)x.Value)
             };
+        }
+
+        public static BaseResult<T> Failure(string error)
+        {
+            var result = new BaseResult<T>();
+            result.AddGeneralError(error);
+
+            return result;
         }
     }
 }
